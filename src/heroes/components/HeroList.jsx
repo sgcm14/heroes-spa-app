4dffffd7b6b2dc93b8f08526f9c69ad37813930a
sgcm14 <sgcm14@gmail.com>
2025-07-24 19:50:49 -0500

    add search page

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,12 +1,14 @@
+import { useMemo } from "react";
 import { getHeroesByPublisher } from "../helpers";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
-    <ul>
+    <div className="row rows-cols-1 row-cols-md-3 g-3">
       {heroes.map((hero) => (
-        <li key={hero.id}>{hero.superhero}</li>
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -34,6 +34,14 @@ export const Navbar = () => {
           >
             DC
           </NavLink>
+           <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "active" : ""}`
+            }
+            to="/search"
+          >
+            Search
+          </NavLink>
         </div>
       </div>
 
