b8f5311cfca1fcc049f24e25e09bbcd31ea24046
sgcm14 <sgcm14@gmail.com>
2025-07-30 11:29:49 -0500

    fix environment again deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/heroes-spa/" : "/", // 👈 clave aquí
+  base: command === "build" ? "/heroes-spa-app/" : "/", // 👈 clave aquí
   plugins: [react()],
 }));
